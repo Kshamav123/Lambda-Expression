14d2394293404244d8898366ecd93143f18e8af9
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-22 16:31:05 +0530

    Passing lambda function to show result

--- IMathFunction.java
+++ IMathFunction.java
@@ -3,5 +3,8 @@ package com.lambdaexpression;
 @FunctionalInterface
 public interface IMathFunction {
 	int calculate(int a,int b);
-
+	
+	static void printResult(int a, int b, String function, IMathFunction obj) {
+		System.out.println("Result of " + function + " is : " + obj.calculate(a, b));
+	}
 }
